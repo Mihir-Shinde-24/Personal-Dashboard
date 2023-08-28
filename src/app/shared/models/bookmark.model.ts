8279d3967ecc679fe7a09723d8f7ae181661af03
Mihir Shinde <mihir.s@grtship.com>
2023-08-28 21:04:10 +0530

    18-creating-bookmark-service completed

--- src/app/shared/models/bookmark.model.ts
+++ src/app/shared/models/bookmark.model.ts
@@ -3,9 +3,11 @@ import { v4 as uuidv4 } from 'uuid';
 export class Bookmark{
 
     id!:string;
-
-    constructor(public name:string, public url: URL) {
+    url!:URL;
+    constructor(public name:string, url: string) {
         this.id = uuidv4();
+        this.url = new URL(url);
+
     }
 
 
