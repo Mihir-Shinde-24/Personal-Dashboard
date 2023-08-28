6e1dd6d569a89e30bdb80536cbca74c707fd4823
Mihir Shinde <mihir.s@grtship.com>
2023-08-28 20:18:47 +0530

    17-delete-animation-todo-item completed

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
 import {Todo} from "../../shared/models/todo.model";
 import {TodoService} from "../../shared/services/todo.service";
+import {animate, style, transition, trigger} from "@angular/animations";
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
-  styleUrls: ['./todos.component.scss']
+  styleUrls: ['./todos.component.scss'],
+  animations: [
+      trigger('todoItemAnim',[
+          transition(':leave',[
+              animate(280,style({
+                opacity: 0,
+                height: 0,
+                marginBottom:0
+              }))
+          ])
+      ])
+  ]
 })
 export class TodosComponent {
 
